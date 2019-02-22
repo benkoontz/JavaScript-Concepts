afd5782d0bce7b202bec79f4973fe30f5c42fc2f
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-22 14:22:11 -0700

    Create ben.js

---
+++ 05_Concatenate Strings/ben.js
@@ -0,0 +1,11 @@
+// concatenate strings
+
+var myStr = "first string" + " second string";
+console.log(myStr)
+
+// += operator to concatenate
+
+var str1 = "first"
+str1 += " second"
+
+console.log(str1)

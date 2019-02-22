9590f149c468e484e1372947fe2a963d6c1e06cd
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-22 14:19:02 -0700

    Create ben.js

---
+++ 04_Escape Sequences/ben.js
@@ -0,0 +1,21 @@
+// escape squeneces
+
+// escape character to pring the sentence
+var myStr = "i am a \"double quoted\" string"; 
+console.log(myStr)
+
+// escape a single quote
+var address = '<a href = "google.com\"';
+console.log(address)
+
+/*
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+**/
+

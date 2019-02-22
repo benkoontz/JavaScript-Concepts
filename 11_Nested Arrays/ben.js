649607423b46f6dd5fcd5314d440e4cf8d8eb81b
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-22 14:44:50 -0700

    Create ben.js

---
+++ 11_Nested Arrays/ben.js
@@ -0,0 +1,12 @@
+// arrays can store different data types
+
+var array = ["ben", 24]; // elements can be any data type
+
+var myArray = ["hello", 1]
+
+console.log(array)
+console.log(myArray)
+
+// nested or multidimensional array
+var multiArray = [["ben", 20], ["hello", 6]]
+console.log(multiArray)

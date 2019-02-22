40f04234f9a04c001563520881e5806e204d9ee6
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-22 13:57:38 -0700

    Create ben.js

---
+++ 01_Declaring Variables/ben.js
@@ -0,0 +1,9 @@
+var number = 5; // in line comment
+
+var myName = "Ben" // var is used throughout whole program
+
+myName = 8
+
+let ourname = "ben2" // used within a scope
+
+const pi = 3.14 // variable that never changes

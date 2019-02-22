982c5d1581f779c1585a8fcd0c9af3a1e854170c
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-22 13:57:18 -0700

    Delete ben.js

--- declaring variables/ben.js
+++
@@ -1,9 +0,0 @@
-var number = 5; // in line comment
-
-var myName = "Ben" // var is used throughout whole program
-
-myName = 8
-
-let ourname = "ben2" // used within a scope
-
-const pi = 3.14 // variable that never changes

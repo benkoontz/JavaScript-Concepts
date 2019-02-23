584f76405979e4a9ca1affb173c21adbba5bf58d
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-23 12:46:24 -0700

    Update ben.js

--- 22_Changing Objects/ben.js
+++ 22_Changing Objects/ben.js
@@ -1,1 +1,21 @@
 
+
+// changing object property
+var dog = {
+    "name": "ben",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything"],
+    7 : 8
+};
+
+dog.name = "hello"
+console.log(dog.name) // prints hello
+
+// changing object properties
+dog.bark = "bow wow" // adds new property bark
+console.log(dog.bark) // prints bow wow
+
+// delete property object
+delete dog.bark;
+console.log(dog.bark) // prints null

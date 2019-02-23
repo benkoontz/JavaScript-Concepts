b59ba867d87980989c0a3c57f4989ebabc879b2d
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-23 13:43:38 -0700

    Create ben.js

---
+++ 28_For loop to Add Array Values/ben.js
@@ -0,0 +1,10 @@
+// while loop, iterate through an array
+
+var array = [9, 10, 11, 12];
+var total = 0;
+
+for(var i = 0; i < array.length; i++) {
+    total += array[i]; // sum up the values of an arrary
+}
+
+console.log(total); // prints 42

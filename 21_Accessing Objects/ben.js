adc83d8214f61c82e1f792965362a322910e26be
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-23 12:41:31 -0700

    Update ben.js

--- 21_Accessing Objects/ben.js
+++ 21_Accessing Objects/ben.js
@@ -4,13 +4,15 @@ var dog = {
     "name": "ben",
     "legs": 4,
     "tails": 1,
-    "friends": ["everything"]
+    "friends": ["everything"],
+    7 : 8
 };
 
+var hello = dog[7]
 var ben = dog.name
 var hi = dog.friends
 var yep = dog["tails"];
 var nope = "tails";
 var tails = dog[nope];
 
-console.log(ben + " " + hi + " " + yep + " " + tails) // prints ben everything 1 1
+console.log(ben + " " + hi + " " + yep + " " + tails + " " + hello) // prints ben everything 1 1 8

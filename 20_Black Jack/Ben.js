c689d28233ae6227943ce552c75e0c90acb001c6
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-23 12:32:44 -0700

    Create Ben.js

---
+++ 20_Black Jack/Ben.js
@@ -0,0 +1,32 @@
+// Black Jack card game Example
+
+var count = 0;
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count ++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count --;
+            break;
+    }
+    
+    var holdbet = 'Hold'
+    if(count > 0) {
+        holdbet = "bet";
+    }
+    
+    return count + " " + holdbet;
+}
+
+cc(2); cc('K'); cc(2); cc('K'); cc ('A');
+console.log(cc())

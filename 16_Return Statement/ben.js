317e2152249f11cfb61da9fb7243f865bd2b9b12
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-23 11:39:34 -0700

    Create ben.js

---
+++ 16_Return Statement/ben.js
@@ -0,0 +1,14 @@
+// return statement
+
+function subtract(num) {
+    return num - 7;
+}
+
+console.log(subtract(8));
+
+// functions don't have to have return statements
+function add(sum) {
+    console.log(sum + 3);
+}
+
+add(3);

3afd040521b4671b8ddf5b751723b54430b0b635
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-24 16:03:01 -0700

    Create ben.js

---
+++ 32_Parse Int/ben.js
@@ -0,0 +1,9 @@
+// parse int
+
+function convertToInteger(str) {
+    return parseInt(str)
+    //return parseInt(str, 2) // base 2, converts binary to number
+}
+
+console.log(convertToInteger("56"))
+//console.log(convertToInteger("01011")) // returns 11

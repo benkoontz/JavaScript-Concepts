0df251b0b35717f8b187664ba9cae7dbdd9f0fcb
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-25 09:37:30 -0700

    Create ben.js

---
+++ 46_Template Literals/ben.js
@@ -0,0 +1,11 @@
+// template literals
+
+const person = {
+    name: "ben",
+    age: 18
+};
+
+const greeting = `Hello, my name is ${person.name}'!
+I am ${person.age} years old.`;
+
+console.log(greeting) // prints Hello, my name is ben'! I am 18 years old

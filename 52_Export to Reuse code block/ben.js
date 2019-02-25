0bcb5330a50599dfb4e1daaf7593f75934f032a7
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-25 10:17:45 -0700

    Create ben.js

---
+++ 52_Export to Reuse code block/ben.js
@@ -0,0 +1,10 @@
+// export to reuse code block
+
+const capitalizeString = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+export { capitalizeString};
+
+export const foo = "bar"
+export const bar = "foo"

3211e2fd5a370d7758c9b4bacfaed87665d67e88
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-24 17:57:35 -0700

    Create ben.js

---
+++ 41_Destructuring Objects/ben.js
@@ -0,0 +1,15 @@
+// destructuring objects
+
+const temp = {
+    today: 77.5,
+    tomorrow: 79
+};
+
+function getTemp(avgTemp) {
+    "use strict";
+    
+    const {tomorrow : tempOfTomorrrow } = avgTemp;
+    return tempOfTomorrrow
+}
+
+console.log(getTemp(temp))
